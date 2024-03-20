2d805a5f58b169cf9a35dd42961be7fe8f4d37db
chumascalo <aruniorrego@liceovvh.cl>
2024-03-20 10:38:30 -0300

    ok

--- fundamentos JS/02_ejercicios/script.js
+++ fundamentos JS/02_ejercicios/script.js
@@ -1,17 +1,17 @@
 console.log("vinculado correctamente....")
- // Ejercicio 1: Declaración de variables
-    // Instrucciones: Declara dos variables numéricas llamadas 'ahora' y 'fecha_nac'.
-    // Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
-    
-    function declararVariables() {
-        let ahora=2024;
-        let fecha_nac=2006;
-        let resultado = ahora - fecha_nac;
-        alert(`mi edad es:  ${resultado}` );
-    }
-    
-    // Ejercicio 2: Asignación de valores
-    // Instrucciones: Completa la función para asignar el valor de 'var1' a 'var2' y luego muestra el valor de 'var2'.
+// Ejercicio 1: Declaración de variables
+// Instrucciones: Declara dos variables numéricas llamadas 'ahora' y 'fecha_nac'.
+// Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
+
+function declararVariables() {
+    let ahora=2024;
+    let fecha_nac=2006;
+    let resultado = ahora - fecha_nac;
+    alert(`mi edad es:  ${resultado}` );
+}
+
+// Ejercicio 2: Asignación de valores
+// Instrucciones: Completa la función para asignar el valor de 'var1' a 'var2' y luego muestra el valor de 'var2'.
     function asignarValores() {
     let var1 ="hola mundo";
     let var2=var1;
@@ -35,14 +35,30 @@ console.log("vinculado correctamente....")
     // Tu código aquí
     }
     
-    // Ejercicio 4: Manipulación de cadenas
-    // Instrucciones: Completa la función para construir un mensaje utilizando las variables 'direccion' y 'n_casa'.
-    // Luego, muestra la longitud del mensaje y el antepenultimo carácter en el mensaje. (.length) 
-    function manipularCadenas() {
+// Ejercicio 4: Manipulación de cadenas
+// Instrucciones: Completa la función para construir un mensaje utilizando las variables 'direccion' y 'n_casa'.
+// Luego, muestra la longitud del mensaje y el antepenultimo carácter en el mensaje. (.length) 
+function manipularCadenas() {
     let direccion = "Av. Alameda";
     let n_casa="2300";
     let mensaje= `la direccion es: ${direccion}  ${n_casa}`;
     let longitud = mensaje.length;
     alert(`La lognitud del mensaje es: ${mensaje.length} \nCaracter antepenultimo: ${mensaje[mensaje.length-3]}`)
     
+    }
+
+/*•    Crear una función llamada “Suma_indices()”
+•    Crear un arreglo  de largo 7 (1,2,3,4,5,6,7)
+•    Sumar el ultimo valor, con el penúltimo más el 1 valor de las posiciones
+•    Mostrar el resultado en el HTML con un Alert
+*/
+
+function sumarIndices(){
+    let valor = [1,2,3,4,5,,6,7];
+    let ultimoValor = valor[valor.length - 1];
+    let penultimoValor = valor[valor.length -2];
+    let primerValor= valor[0];
+    let resultado = ultimoValor + penultimoValor + primerValor;
+    
+    alert(`el resultado es : ${resultado}`);
     }
