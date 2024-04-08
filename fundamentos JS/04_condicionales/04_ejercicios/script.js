cfdfa9499b5ec72086f59fe5b47aecea1625d0c2
chumascalo <chumascalo>
2024-04-08 10:42:51 -0400

    ok

---
+++ fundamentos JS/04_condicionales/04_ejercicios/script.js
@@ -0,0 +1,22 @@
+// Problema 4: Crea una variable llamada numero que
+// almacene un número. Escribe un programa en JavaScript
+// que determine si el número es positivo, negativo o igual
+// a cero. Imprime el resultado en la consola. Utiliza
+// una declaración "if-else if-else" para resolver este
+// problema
+
+function positivoNegativo(){
+    numero = parseInt(prompt("ingrese un numero"))
+    resultado = ""
+    
+    if (numero < 0 ){
+    resultado = `<h2>${numero}</h2> es negativo`
+    }
+    else if (numero > 0){
+        resultado = `<h2>${numero}</h2> es positivo`
+    }
+    else{
+        resultado = `el numero es <h2>${numero}</h2>`
+    }
+    document.getElementById("lista").innerHTML = resultado
+}
