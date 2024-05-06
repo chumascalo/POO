dea3aa309a9ef8becaa639ddb236bd7ffb828c15
chumascalo <chumascalo>
2024-05-06 14:27:28 -0400

    ok

--- fundamentos JS/06_manipulando_elementos/01_ejercicios_tablas/script.js
+++ fundamentos JS/06_manipulando_elementos/01_ejercicios_tablas/script.js
@@ -3,8 +3,19 @@ function traerTabla(){
     let string = ``;
 
     for (let i = 1; i <= 10; i++){
-    string += `${i} * ${num} = ${ i*num}<br>`
+    string += `${i} * ${num} = ${ i*num}<br>`;
+    }
+
+document.getElementById("tabla").innerHTML = `tabla del ${num} <br>${string}`;
+}
+
+
+function invertirTabla(){
+let stringInv = "";
+let cadena = document.getElementById("cadenaInput").value;
+for (i = cadena.length -1  ; i >= 0; i--){
+    stringInv += cadena[i]; 
 
 }
-document.getElementById("tabla").innerHTML = `tabla del ${num} <br>${string}`;
+document.getElementById("cadenaInvertida").innerHTML = `${stringInv} `;
 }
