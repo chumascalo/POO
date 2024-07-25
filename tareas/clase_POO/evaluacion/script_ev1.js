d84d53eb82409a9ac525fd9cdb22901fdcfabda4
chumascalo <chumascalo>
2024-07-25 11:51:07 -0400

    ok

--- tareas/clase_POO/evaluacion/script_ev1.js
+++ tareas/clase_POO/evaluacion/script_ev1.js
@@ -1,21 +1,24 @@
 /* Crea una función en JavaScript que reciba un array de números y devuelva el número mayor de ese array. La
 función debe usar un bucle for para recorrer el array y encontrar el número más grande. */
-
 function encontrarMayorYmenor(array) {
     let mayor = array[0];
     let menor = array[0];
     
     for (let i = 0; i < array.length; i++) {
         if (array[i] > mayor) {
-            mayor = array[i];
+        mayor = array[i];
         }
         if (array[i] < menor) {
             menor = array[i];
         }
     }
 
-    let resultado = `mayor: ${mayor}  menor: ${menor}`;
-    return resultado;
-}
+    return {
 
-alert(encontrarMayorYmenor([2, 3, 4, 2, 2, 323, 545, 2, 4]));
+    mayor: mayor,
+    menor :menor
+}    
+    }
+
+let resultado = encontrarMayorYmenor([2, 3, 4, 2, 2, 323, 545, 2, 4]);
+alert(`Mayor: ${resultado.mayor}, Menor: ${resultado.menor}`);
