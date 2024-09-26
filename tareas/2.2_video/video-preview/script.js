fdbc94bb2c9244c51a03ec063b132437b6e80c5a
chumascalo <chumascalo>
2024-09-26 12:35:52 -0300

    ok

--- tareas/2.2_video/video-preview/script.js
+++ tareas/2.2_video/video-preview/script.js
@@ -1,17 +1,19 @@
 console.log("page loaded...");
 
+// Función para reproducir el video seleccionado en el reproductor principal
+function playVideo(videoSecundario) {
+    var mainvideo = document.getElementById("mainvideo");  // Obtiene el elemento de video principal
+    var temp = mainvideo.src;  // Guarda temporalmente la fuente del video principal
+    mainvideo.src = videoSecundario.src;  // Cambia la fuente del video principal a la del video secundario
+    mainvideo.play();  // Reproduce el video principal con la nueva fuente
+    videoSecundario.src = temp;  // Intercambia las fuentes, asignando la fuente anterior del video principal al video secundario
+}
 
-function playVideo(video){
-video.play();
-};
-
-function pauseVideo(video){
-video.pause();
-video.currentTime = 0; //vuelve video al inicio
-};
-
-function playPequeños(video) {
-    var largeVideo = document.getElementById('largeVideo');
-    largeVideo.src = video.src;
-    largeVideo.play(); 
+// Función para pausar el video y devolver la fuente original
+function pauseVideo(videoSecundario) {
+    var mainvideo = document.getElementById("mainvideo");  // Obtiene el elemento de video principal
+    var tempSrc = mainvideo.src;  // Guarda temporalmente la fuente del video principal
+    mainvideo.src = videoSecundario.src;  // Cambia la fuente del video principal a la del video secundario
+    videoSecundario.src = tempSrc;  // Intercambia las fuentes nuevamente
+    mainvideo.play();  // Reproduce el video principal con la nueva fuente
 }
