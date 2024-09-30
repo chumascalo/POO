14525049191e02cb7d94762ccb911d78d488eea8
chumascalo <chumascalo>
2024-09-30 10:54:38 -0300

    ok

---
+++ tareas/clase_POO/2.1/script.js
@@ -0,0 +1,10 @@
+// Función para mostrar un mensaje de "me gusta"
+function message() {
+    alert('¡Gracias por tu "me gusta"!');
+}
+
+// Función para ocultar el botón de "Agregar definición"
+function hide(button) {
+    // Ocultar el botón al hacer clic
+    button.style.display = 'none';
+}
